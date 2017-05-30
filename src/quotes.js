e2f2840c1454657300c4aa0ebe53169f062c62ad
Faraz Patankar <farazpatankar@gmail.com>
2017-05-31 03:10:41 +0530

    center everything + more quotes

--- src/quotes.js
+++ src/quotes.js
@@ -4,6 +4,8 @@ const quotes = [
 	"This malicious and unprofessional channel has been persecuting me through repeated broadcasts, abetted by slanderous discussion shows, in which a tissue of lies and distortions is built up to prejudice the mind of the viewer into believing that a murder was committed and that I am either culpable or complicit.",
 	"The chase for TRPs by a new channel trying to establish itself is understandable, but it does not justify such falsehoods and outrageous conduct, which is intended to influence the investigation, derail a fair enquiry and obstruct due process.",
 	"As a vegetarian I object strongly to the ban on cow slaughter. Gandhiji had objected to imposing one's view on others. Freedom is being eroded.",
+	"Hindu fundamentalism is a contradiction in terms, since Hinduism is a religion without fundamentals; there is no such thing as a Hindu heresy. How dare a bunch of goondas shrink the soaring majesty of the Vedas and the Upanishads to the petty bigotry of their brand of identity politics?",
+	"If America is a melting-pot, then to me India is a thali, a selection of sumptuous dishes in different bowls. Each tastes different, and does not necessarily mix with the next, but they belong together on the same plate, and they complement each other in making the meal a satisfying repast.",
 
 ];
 
