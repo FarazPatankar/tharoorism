c1ed07c04349c13993954db4535130b646e66a53
Faraz Patankar <farazpatankar@gmail.com>
2017-05-31 02:50:27 +0530

    stop same quote from showing up twice in a row

--- src/App.js
+++ src/App.js
@@ -5,21 +5,31 @@ import quotes from './quotes'
 class App extends Component {
   constructor() {
     super();
-    this.state={quote: "Click the button for a quote, human!"};
+    this.state={quote: "Click to get a fresh dose of Tharoorism, human!"};
     this.handleClick = this.handleClick.bind(this);
+    this.lastQuote = -1;
   }
 
   handleClick() {
+    let quoteNumber = this.generateQuoteNumber();
+    while(quoteNumber === this.state.lastQuote) {
+      quoteNumber = this.generateQuoteNumber();
+    }
+
     this.setState({
-      quote: quotes[Math.floor(Math.random() * quotes.length)]
+      quote: quotes[quoteNumber]
     })
+  }
+
+  generateQuoteNumber() {
+    return Math.floor(Math.random() * quotes.length);
   }
   render() {
     return (
       <div className="App">
         <div className="App-header">
           <h1>{this.state.quote}</h1>
-          <a href="#" className="button" onClick={this.handleClick}>Click To Generate Random Quote</a>
+          <a href="#" className="button" onClick={this.handleClick}>Give Me My Dose Of Tharoorism!</a>
         </div>
       </div>
     );
