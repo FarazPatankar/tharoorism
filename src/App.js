09cb93884fb84c19569780954a2c57a73305ed4e
Faraz Patankar <farazpatankar@gmail.com>
2017-06-02 16:24:13 +0530

    [Faraz] fancy typing animation intro

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 import quotes from './quotes'
+import Typist from 'react-typist';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('UA-100343895-1')
+
+function fireTracking() {
+  ReactGA.set({ page: window.location.pathname + window.location.search });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+
 
 class App extends Component {
   constructor() {
     super();
-    this.state={quote: "Click to get a fresh dose of Tharoorism, human!"};
+    this.state={
+      quote: "",
+      lastQuote: -1,
+      isTypingDone: false
+    };
     this.handleClick = this.handleClick.bind(this);
-    this.lastQuote = -1;
+  }
+
+  componentDidMount() {
+    fireTracking();
   }
 
   handleClick() {
@@ -25,13 +42,41 @@ class App extends Component {
   generateQuoteNumber() {
     return Math.floor(Math.random() * quotes.length);
   }
+
+  typingDone() {
+    setTimeout(() => {
+      if(this.state.isTypingDone === false) {
+        this.setState({
+          isTypingDone: true
+        })
+      }
+    }, 2000)
+  }
+
   render() {
+    const typingDone = this.state.isTypingDone;
+    let element = null;
+
+    if (!typingDone) {
+      element = <Typist
+                  className="typist-text"
+                  avgTypingDelay={80}
+                  startDelay={1500}
+                  cursor={{hideWhenDone: true}}
+                  onTypingDone={this.typingDone.bind(this)}
+                >
+                  Welcome to Tharoorism, Mortal!
+                </Typist>
+    } else {
+      element = <div className="App-header">
+                  <h1>{this.state.quote}</h1>
+                  <a href="#" className="button" onClick={this.handleClick}>Give Me My Dose Of Tharoorism!</a>
+                </div>
+    }
+
     return (
       <div className="App">
-        <div className="App-header">
-          <h1>{this.state.quote}</h1>
-          <a href="#" className="button" onClick={this.handleClick}>Give Me My Dose Of Tharoorism!</a>
-        </div>
+        {element}
       </div>
     );
   }
